6221203e3a1a9e4c3b8637cda8ce3a76a0861439
Inkpear <2754919327@qq.com>
2025-03-14 18:27:15 +0800

    同步代码

--- main_server/src/main.rs
+++ main_server/src/main.rs
@@ -5,6 +5,12 @@ use actix_web::{App, web, HttpServer};
 #[path="utils/http_services.rs"]
 mod http_services;
 
+#[path="utils/reporter_services.rs"]
+mod reporter_services;
+
+#[path="utils/cq_models.rs"]
+mod cq_models;
+
 mod models;
 mod handler;
 mod router;
