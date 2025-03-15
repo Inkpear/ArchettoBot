43ae13d0b261eb410966907ff69927c61d9de1a9
Inkpear <2754919327@qq.com>
2025-03-15 16:26:00 +0800

    同步代码

--- main_server/src/main.rs
+++ main_server/src/main.rs
@@ -1,47 +1,42 @@
-use std::io;
 use crate::models::Config;
-use http_services::HttpServices;
 use actix_web::{web, App, HttpServer};
+use http_services::HttpServices;
+use std::io;
 
-
-#[path="utils/http_services.rs"]
+#[path = "utils/http_services.rs"]
 mod http_services;
 
-#[path="utils/scheduled_task_services.rs"]
+#[path = "utils/scheduled_task_services.rs"]
 mod scheduled_task_services;
 
-#[path="utils/cq_models.rs"]
+#[path = "utils/cq_models.rs"]
 mod cq_models;
 
-mod models;
 mod handler;
+mod models;
 mod router;
 mod state;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    let config = Config::from_path("./config.yaml")
-        .unwrap_or_else(|_| {
-            let config = Config::new();
-            let _ = config.save();
-            config
-        });
-    
-    let http_services = web::Data::new(HttpServices::builder()
-        .bot_server(config.bot_server_addr())
-        .crawler_server(config.crawler_server_addr())
-        .build()
-        .unwrap());
-    
-    let app = {
-        move || {
-            App::new()
-            .app_data(http_services.clone())
-        }
-    };
+    let config = Config::from_path("./config.yaml").unwrap_or_else(|_| {
+        let config = Config::new();
+        let _ = config.save();
+        config
+    });
+
+    let http_services = web::Data::new(
+        HttpServices::builder()
+            .bot_server(config.bot_server_addr())
+            .crawler_server(config.crawler_server_addr())
+            .build()
+            .unwrap(),
+    );
+
+    let app = { move || App::new().app_data(http_services.clone()) };
 
     HttpServer::new(app)
-    .bind(config.main_server_addr())?
-    .run()
-    .await   
+        .bind(config.main_server_addr())?
+        .run()
+        .await
 }
--- main_server/src/state.rs
+++
@@ -1,7 +0,0 @@
-use tokio::sync::{RwLock, Mutex};
-use std::sync::Arc;
-use crate::http_services::HttpServices;
-
-pub struct AppState {
-    http_service: Arc<HttpServices>,
-}
